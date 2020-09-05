94ac29beccdf880f6b0bb8d1310634297436dbd2
Yuko IINUMA <iinuma@dgx1a.hpc.vpl.nii.ac.jp>
2020-09-05 21:34:32 +0900

    mAP を計算するコードを変更

--- retrieval_v2.py
+++ retrieval_v2.py
@@ -42,39 +42,55 @@ def make_rank(query_idx):
 
 
 # 全クエリの検索を行う
-def retrieval(gallery_file, query_file, save_file):
-    # gallery と query の読み込み
-    global gallery_fvs
-    gallery_fvs = np.load(gallery_file)
-    global query_fvs
-    query_fvs = np.load(query_file)
-    # metric が l2+norm なら，それぞれ正規化
-    if args.metric == "l2+norm":
-        gallery_fvs = gallery_fvs / np.array([np.sqrt(np.sum(gallery_fvs ** 2, axis=1))]).T
-        query_fvs = query_fvs / np.array([np.sqrt(np.sum(query_fvs ** 2, axis=1))]).T
-    
-    # クエリごとの検索を並列で処理する
-    parallel_args = list(range(len(query_fvs)))
-    cpu_num = min(len(parallel_args), int(cpu_count() * 0.8))
-    print("Start retrieval : {} queries, {} CPU cores".format(len(parallel_args), cpu_num))
-    with Pool(cpu_num) as pool:
-        imap = pool.imap(make_rank, parallel_args)
-        result = list(tqdm(imap, total=len(parallel_args)))
-        rankings = np.array([r[1] for r in sorted(result)])
-    print("Retrieval has been finished. Result is saved in", save_file)
+def retrieval():
+    global rankings
 
-    # 検索結果を保存してランキングを返す
-    np.save(save_file, rankings)
+    start = 0
+    interval = 10000
+    aps = []
+
+    # クエリごとの検索を1万件ずつ並列で処理する
+    query_indexes = list(range(len(query_fvs)))
+    cpu_num = min(len(query_indexes), int(cpu_count() * 0.8))
+    print("Start retrieval : {} queries, {} CPU cores".format(len(query_indexes), cpu_num))
+
+    while start < len(query_indexes):
+        # 並列処理の設定
+        parallel_args = query_indexes[start:start+interval]
+        
+        # ランキングの作成
+        with Pool(cpu_num) as pool:
+            imap = pool.imap(make_rank, parallel_args)
+            result = list(tqdm(imap, total=len(parallel_args)))
+            new_rankings = np.array([r[1] for r in sorted(result)])
+        print("Retrieval has been finished. {} / {}".format(start + len(parallel_args), len(query_indexes)))
+
+        # ランキングの保存
+        if os.path.exists(args.ranking_file):
+            prev_rankings = np.load(args.ranking_file)
+            rankings = np.concatenate([prev_rankings, new_rankings])
+        else:
+            rankings = new_rankings
+        np.save(args.ranking_file, rankings)
+
+        # validation フラグが true なら mAP を計算する
+        if args.validation:
+            with Pool(cpu_num) as pool:
+                imap = pool.imap(calc_ap, parallel_args)
+                result = list(tqdm(imap, total=len(parallel_args)))
+                aps += result
+            print("mAP : {} ({} queries)".format(np.mean(np.array(aps)), start + len(parallel_args)))
+        
+        start += interval
+
     return rankings
 
 
-# 動作と人物による検索結果をマージする
-def merge_ranking(action_rankings, person_rankings):
-    return action_rankings
-
-
 # Average Precision の計算
-def calc_ap(gt_label, ranking, gallery_dict):
+def calc_ap(query_idx):
+    gt_label = query_dict[query_idx]
+    ranking = rankings[query_idx][:args.limit]
+
     correct_num = 0
     total_num = 0
     precision_sum = 0.0
@@ -89,69 +105,57 @@ def calc_ap(gt_label, ranking, gallery_dict):
     return ap
 
 
-# calc_ap 関数を並列実行するためのラッパー
-def wrapper_calc_ap(arg):
-    gt_label, ranking, gallery_dict = arg
-    ap = calc_ap(gt_label, ranking, gallery_dict)
-    return ap
-
-
 # mean Average Precision の計算
-def calc_map(rankings, gallery_dict, query_dict):
+def calc_map(query_label_dict):
     """
-    gallery_dict : { feature_vector_idx : label }
-    query_dict : { lebel : [feature_vector_idx] }
+    query_label_dict : { lebel : [feature_vector_idx] }
     """
     ap_dict = {}
-    mean_ap = 0.0
-    for label in query_dict:
+    aps = []
+    
+    for label in query_label_dict:
         # 同じラベルの映像については並列に AP を計算する
-        parallel_args = [[label, rankings[idx], gallery_dict] for idx in query_dict[label]]
+        parallel_args = query_label_dict[label]
         cpu_num = min(len(parallel_args), int(cpu_count() * 0.8))
-        pool = Pool(cpu_num)
-        result = np.array(pool.map(make_rank, parallel_args))
-        pool.close()
+        with Pool(cpu_num) as pool:
+            imap = pool.imap(calc_ap, parallel_args)
+            result = list(tqdm(imap, total=len(parallel_args)))
+            aps += result
 
         # ラベルごとの AP
-        label_ap = np.mean(result)
+        label_ap = np.mean(np.array(result))
         ap_dict[label] = label_ap
-        mean_ap += label_ap / len(query_dict)
+        print("{} class AP : {} ({} queries)".format(label, label_ap, len(parallel_args)))
+
+    mean_ap = np.mean(np.array(aps))
+    print("All class mAP : {} ({} queries)".format(label_ap, len(aps)))
 
     return mean_ap, ap_dict
 
 
-def load_gallery_data(csv_file):
-    action_gallery_dict = {}
-    person_gallery_dict = {}
+def load_index_data(csv_file):
+    action_index_dict = {}
     with open(csv_file, "r") as fp:
         reader = csv.reader(fp, delimiter=' ')
         for r in reader:
             if r[3] != '' and r[6] != '':
-                action_gallery_dict[r[3]] = r[6]
-            if r[4] != '' and r[7] != '':
-                action_gallery_dict[r[4]] = r[7]
+                action_index_dict[int(r[3])] = int(r[6])
     
-    return action_gallery_dict, person_gallery_dict
+    return action_index_dict
 
 
-def load_query_data(csv_file):
-    action_query_dict = {}
-    person_query_dict = {}
+def load_label_data(csv_file):
+    action_label_dict = {}
     with open(csv_file, "r") as fp:
         reader = csv.reader(fp, delimiter=' ')
         for r in reader:
             if r[3] != '' and r[6] != '':
-                if r[6] in action_query_dict:
-                    action_query_dict[r[6]].append(r[3])
+                if r[6] in action_label_dict:
+                    action_label_dict[int(r[6])].append(int(r[3]))
                 else:
-                    action_query_dict[r[6]] = [r[3]]
-            if r[4] != '' and r[7] != '':
-                if r[4] in action_query_dict:
-                    action_query_dict[r[7]].append(r[4])
-                else:
-                    action_query_dict[r[7]] = [r[4]]
+                    action_label_dict[int(r[6])] = [int(r[3])]
 
-    return action_query_dict, person_query_dict
+    return action_label_dict
 
 
 # 引数の整理
@@ -163,6 +167,12 @@ def parse_args():
                         type=str,
                         required=True,
                         help="Path to data directory"
+                       )
+    # 検索結果を保存するファイルの指定
+    parser.add_argument('--ranking_file',
+                        type=str,
+                        required=True,
+                        help="Path to the file saving result ranking"
                        )
     # 特徴ベクトルの距離指標
     parser.add_argument('--metric',
@@ -182,17 +192,17 @@ def parse_args():
                         action="store_true",
                         help="Validate retrieval results or not"
                        )
+    # 評価のみを行う場合
+    parser.add_argument('--validation_only',
+                        action="store_true",
+                        help="Validate retrieval results or not"
+                       )
 
     args = parser.parse_args()
     args.gallery_features_csv = os.path.join(args.data_dir, "gallery/features.csv")
     args.gallery_action_features_npy = os.path.join(args.data_dir, "gallery/action_features.npy")
-    args.gallery_person_features_npy = os.path.join(args.data_dir, "gallery/person_features.npy")
     args.query_features_csv = os.path.join(args.data_dir, "query/features.csv")
     args.query_action_features_npy = os.path.join(args.data_dir, "query/action_features.npy")
-    args.query_person_features_npy = os.path.join(args.data_dir, "query/person_features.npy")
-    args.action_ranking_npy = os.path.join(args.data_dir, "query/action_ranking.npy")
-    args.person_ranking_npy = os.path.join(args.data_dir, "query/person_ranking.npy")
-    args.total_ranking_npy = os.path.join(args.data_dir, "query/total_ranking.npy")
 
     # 最低限必要なファイルがあるかどうかを確認する
     if not os.path.exists(args.gallery_features_csv):
@@ -204,43 +214,43 @@ def parse_args():
     if not os.path.exists(args.query_action_features_npy):
         raise NoFileExistsError("{} does not exist.".format(args.query_action_features_npy))
 
+    # 評価のみを行う指定で，ranking_flle が存在しないときはエラー
+    if args.validation_only and not os.path.exists(args.ranking_file):
+        raise NoFileExistsError("{} does not exist.".format(args.ranking_file))
+
     return args
 
 
 if __name__ == "__main__":
+    # 引数の整理，ファイルの設定
     args = parse_args()
-
-    # 動作特徴量による検索
-    if not os.path.exists(args.action_ranking_npy):
-        action_rankings = retrieval(
-            args.gallery_action_features_npy, 
-            args.query_action_features_npy,
-            args.action_ranking_npy
-        )
-    else:
-        action_rankings = np.load(args.action_ranking_npy)
-        print("Acrion retrieval already has done.")
-    # 人物特徴量のファイルがあれば人物特徴量だけで検索をかけ，動作特徴量の検索結果にマージする
-    if os.path.exists(args.gallery_person_features_npy) and os.path.exists(args.query_peron_features_npy):
-        if not os.path.exists(args.person_ranking_npy):
-            person_rankings = retrieval(
-                args.gallery_person_features_npy, 
-                args.query_person_features_npy,
-                args.person_ranking_npy
-            )
-            rankings = merge_ranking(action_rankings, person_rankings)
-        else:
-            rankings = np.load(args.total_ranking_npy)
-            print("Person retrieval already has done.")
-    else:
-        rankings = action_rankings
     
-    # validation フラグが True なら mAP の計算を行う
-    if args.validation:
-        gallery_dict, _ = load_gallery_data(args.gallery_features_csv)
-        query_dict, _ = load_query_data(args.query_features_csv)
+    # validation_only フラグが True なら mAP の計算のみを行う
+    if args.validation_only or os.path.exists(args.ranking_file):
+        gallery_dict = load_index_data(args.gallery_features_csv)
+        query_dict = load_index_data(args.query_features_csv)
+        rankings = np.load(args.ranking_file)
 
-        mean_ap, _ = calc_map(rankings, gallery_dict, query_dict)
+        print("Start validation.")
+        query_label_dict = load_label_data(args.query_features_csv)
+        mean_ap, ap_dict = calc_map(query_label_dict)
+    else:
+        # gallery と query の読み込み
+        gallery_fvs = np.load(args.gallery_action_features_npy)
+        query_fvs = np.load(args.query_action_features_npy)
+
+        # metric が l2+norm なら，それぞれ正規化
+        if args.metric == "l2+norm":
+            gallery_fvs = gallery_fvs / np.array([np.sqrt(np.sum(gallery_fvs ** 2, axis=1))]).T
+            query_fvs = query_fvs / np.array([np.sqrt(np.sum(query_fvs ** 2, axis=1))]).T
+        
+        # 評価のために action の index と GT label の辞書を作る
+        if args.validation:
+            gallery_dict = load_index_data(args.gallery_features_csv)
+            query_dict = load_index_data(args.query_features_csv)
+
+        # 動作特徴量による検索
+        action_rankings = retrieval()
 
 
 
